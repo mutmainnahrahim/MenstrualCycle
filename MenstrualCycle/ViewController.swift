d1e1b631bd35a89d2fe05ee3ba5506216f4fa07b
Nur Mutmainnah Rahim <18319006@std.stei.itb.ac.id>
2022-04-27 14:06:47 +0700

    Initial Commit

---
+++ MenstrualCycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MenstrualCycle
+//
+//  Created by Nur Mutmainnah Rahim on 27/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
